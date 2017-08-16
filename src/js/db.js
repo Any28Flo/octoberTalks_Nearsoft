7c8062a1e6dd16acc1fd0fb71d2a3ad4d7b4a324
Analin Flores <foglzerika@gmail.com>
2017-08-16 21:32:00 +0200

    Creando bd desde localstorage

--- src/js/db.js
+++ src/js/db.js
@@ -9,34 +9,66 @@
     messagingSenderId: "684858871485"
   };
   firebase.initializeApp(config);
-</script>
-
 var usuariosDb = {
   usuarios:[],
-  // recompensas: []
+  charlas:[]
 
 
 }
 var usuarios     = firebase.database().ref("usuarios");
+var charlas     = firebase.database().ref("/");
 
-var insertaUsuario = function(e){
-e.preventDefault();
+var insertaUsuario = function(){
+    var nombre = localStorage.user;
 
-var nombre = localStorage.user;
-var email = localStorage.email;
+    var email = localStorage.email;
 
-usuarios.set({
-  "nombre": nombre,
-  "email" : email
+    usuarios.push({
+      "nombre": nombre,
+      "email" : email
 
-});
+    });
 // setTimeout(function(){location.href="../views/principal.html" }, 1000);
 
 };
+var insertaCharlas = function(){
+  var lista_de_talleres = [];
+    var nombre = localStorage.user;
+    var email = localStorage.email;
+    var arrayTalleres= localStorage.arregloEventosAgregados;
+    var objTalleres = JSON.parse(arrayTalleres);
+    var descripcioonUsuario = {
+        nombre ,
+        email ,
+        lista_de_talleres
+    }
 
-$(window).load(function () {
-  insertaUsuario();
-  // $("#formulario_recompensas").submit(enviaRecompensa);
-  // $("#registroNuevoUsuario").submit(insertaUsuario);
 
+    for (var i= 0 ; i< objTalleres.length ; i++){
+
+      var titulo= objTalleres[i].titulo;
+      var tipo= objTalleres[i].tipo;
+      var lugar =  objTalleres[i].lugar;
+      var duracion =  objTalleres[i].duracion;
+      var taller ={
+        tituloCharla: titulo,
+        tipoCharla : tipo,
+        lugarCharla:lugar,
+        duracionCharla:duracion
+      }
+      lista_de_talleres.push(taller);
+    }
+    descripcioonUsuario.nombre=nombre;
+    descripcioonUsuario.email = email;
+    descripcioonUsuario.lista_de_talleres = lista_de_talleres;
+
+
+    usuarios.push({
+       "usuario":descripcioonUsuario
+    })
+
+};
+$( document ).ready(function() {
+      // insertaUsuario();
+      insertaCharlas();
 });
